30ad772ca8f40e4f8df914e52b971b73ba840dc8
ABDULLAH TATAN <WPU0001@corp.wpu.com>
2026-08-15 11:10:14 +0300

    fixing bug

--- src/pyspecification/readers.py
+++ src/pyspecification/readers.py
@@ -10,4 +10,11 @@ def read_expression(rule_dict: dict[str, Any]) -> Expression:
     if "name" in rule_dict:
         return PredicateSchema(**rule_dict)
 
+    if len(rule_dict.keys()) > 1:
+        return ConditionExpressionSchema(
+            expressions=[
+                SimplePredicateSchema(root={key: value}) for key, value in rule_dict.items()
+            ]
+        )
+
     return SimplePredicateSchema(root=rule_dict)
