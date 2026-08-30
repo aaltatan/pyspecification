83afc3eb24318597cc74b1b9fbe21a0359f4c347
aaltatan <a.altatan@gmail.com>
2026-08-30 23:21:16 +0300

    add operator type

--- src/pyspecification/__init__.py
+++ src/pyspecification/__init__.py
@@ -1,7 +1,7 @@
 from .compilers import ExpressionWrapperDict, PredicateCompiler, PredicateDict
 from .exceptions import RuleNotFoundError
 from .json_schema import get_json_schema
-from .predicate import Predicate
+from .predicate import OperatorType, Predicate
 from .registry import ObjectRulesRegistry, RuleAlreadyRegisteredError, SubscriptableRulesRegistry
 from .rules import object_rule, subscriptable_rule
 from .schemas import RuleSchema
@@ -9,6 +9,7 @@ from .schemas import RuleSchema
 __all__ = [
     "ExpressionWrapperDict",
     "ObjectRulesRegistry",
+    "OperatorType",
     "Predicate",
     "PredicateCompiler",
     "PredicateDict",
