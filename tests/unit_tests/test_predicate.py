ee461e9cabba085cd8ebdf6fd6edb08037caaf09
aaltatan <a.altatan@gmail.com>
2026-08-30 23:21:07 +0300

    cover bitwise test cases

--- tests/unit_tests/test_predicate.py
+++ tests/unit_tests/test_predicate.py
@@ -1,7 +1,10 @@
+from collections.abc import Callable
 from dataclasses import dataclass
 
 import pytest
-from pyspecification import Predicate
+from pyspecification import OperatorType, Predicate
+
+type RulesCreatorFn = Callable[[OperatorType], dict[str, Predicate[User, bool]]]
 
 
 @dataclass
@@ -11,34 +14,66 @@ class User:
     is_admin: bool = True
 
 
-def _is_admin(user: User) -> bool:
-    return user.is_admin is True
+@pytest.fixture
+def rules_creator() -> RulesCreatorFn:
+    def is_admin(user: User) -> bool:
+        return user.is_admin is True
+
+    def name_istartswith_admin(user: User) -> bool:
+        return user.name.lower().startswith("admin")
+
+    def is_adult(user: User) -> bool:
+        return user.age >= 18 and user.age <= 30
+
+    def inner(operator: OperatorType) -> dict[str, Predicate[User, bool]]:
+        return {
+            "is_admin": Predicate(is_admin, operator=operator),
+            "name_istartswith_admin": Predicate(name_istartswith_admin, operator=operator),
+            "is_adult": Predicate(is_adult, operator=operator),
+        }
+
+    return inner
 
 
-def _name__istartswith_admin(user: User) -> bool:
-    return user.name.lower().startswith("admin")
+@pytest.fixture
+def logical_rules(rules_creator: RulesCreatorFn) -> dict[str, Predicate[User, bool]]:
+    return rules_creator("logical")
 
 
-def _age__between_18_and_30(user: User) -> bool:
-    return user.age >= 18 and user.age <= 30
+@pytest.fixture
+def bitwise_rules(rules_creator: RulesCreatorFn) -> dict[str, Predicate[User, bool]]:
+    return rules_creator("bitwise")
 
 
-is_admin: Predicate[User, bool] = Predicate(_is_admin, operator="logical")
-name__istartswith_admin: Predicate[User, bool] = Predicate(
-    _name__istartswith_admin, operator="logical"
-)
-age__between_18_and_30: Predicate[User, bool] = Predicate(
-    _age__between_18_and_30, operator="logical"
-)
+@pytest.fixture
+def logical_rule(logical_rules: dict[str, Predicate[User, bool]]) -> Predicate[User, bool]:
+    return logical_rules["is_admin"] | (
+        logical_rules["name_istartswith_admin"]
+        & logical_rules["is_adult"]
+        & ~logical_rules["is_admin"]
+    )
 
 
-rule = is_admin | (name__istartswith_admin & age__between_18_and_30 & ~is_admin)
+@pytest.fixture
+def bitwise_rule(bitwise_rules: dict[str, Predicate[User, bool]]) -> Predicate[User, bool]:
+    return bitwise_rules["is_admin"] | (
+        bitwise_rules["name_istartswith_admin"]
+        & bitwise_rules["is_adult"]
+        & ~bitwise_rules["is_admin"]
+    )
 
 
-def test_predicate_repr() -> None:
+def test_repr(
+    logical_rule: Predicate[User, bool],
+    bitwise_rule: Predicate[User, bool],
+) -> None:
     assert (
-        repr(rule)
-        == "Predicate((_is_admin OR ((_name__istartswith_admin AND _age__between_18_and_30) AND NOT _is_admin)))"  # noqa: E501
+        repr(logical_rule)
+        == "Predicate((is_admin OR ((name_istartswith_admin AND is_adult) AND NOT is_admin)))"
+    )
+    assert (
+        repr(bitwise_rule)
+        == "Predicate((is_admin | ((name_istartswith_admin & is_adult) & ~is_admin)))"
     )
 
 
@@ -51,5 +86,20 @@ def test_predicate_repr() -> None:
         (User(name="admin", age=25, is_admin=False), True),
     ],
 )
-def test_is_admin_rule(user: User, expected: bool) -> None:  # noqa: FBT001
-    assert rule(user) == expected
+def test_predicate(
+    logical_rule: Predicate[User, bool],
+    bitwise_rule: Predicate[User, bool],
+    user: User,
+    expected: bool,  # noqa: FBT001
+) -> None:
+    assert logical_rule(user) == expected
+    assert bitwise_rule(user) == expected
+
+
+def test_raising_error_when_combine_bitwise_and_logical() -> None:
+    with pytest.raises(ValueError) as exc:
+        _ = Predicate(lambda _: True, operator="bitwise") | Predicate(
+            lambda _: True, operator="logical"
+        )
+
+    assert "Cannot combine predicates with different operators" in str(exc.value)
