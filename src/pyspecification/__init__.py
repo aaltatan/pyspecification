ce2ced6bb02c35c46bf0385938e0e620f71a0c30
aaltatan <a.altatan@gmail.com>
2026-09-04 11:35:25 +0300

    add docstring

--- src/pyspecification/__init__.py
+++ src/pyspecification/__init__.py
@@ -1,3 +1,9 @@
+"""
+A lightweight, typed Python library for composing business rules as reusable, executable predicates.
+
+This project was inspired by the work and ideas shared by [ArjanCodes](https://github.com/arjancodes), especially the concepts demonstrated in his video: ["The Most Overengineered Python Pattern I've Ever Built"](https://youtu.be/KqfMiuL3cx4?si=WAn01N2I0OO3KOgc).
+"""  # noqa: E501
+
 from .compilers import ExpressionWrapperDict, PredicateCompiler, PredicateDict
 from .exceptions import (
     ArgumentError,
