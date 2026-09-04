d9b5b218d8ca821aaf2f7d420a1e60772eb81609
aaltatan <a.altatan@gmail.com>
2026-09-04 06:18:49 +0300

    add new exception classes

--- src/pyspecification/exceptions.py
+++ src/pyspecification/exceptions.py
@@ -1,8 +1,17 @@
-class RuleNotRegisteredError(Exception):
-    """Exception raised when a rule is not registered in registry class."""
+import re
+from collections.abc import Iterable
 
-    def __init__(self, name: str) -> None:
-        super().__init__(f"Rule '{name}' is not registered")
+
+class RuleDoesNotExistError(Exception):
+    """Exception raised when a rule does not exist."""
+
+    def __init__(self, name: str, available_rules: Iterable[str] | None = None) -> None:
+        msg = f"Rule '{name}' does not exist."
+
+        if available_rules is not None:
+            msg += f" Available rules: {', '.join(available_rules)}"
+
+        super().__init__(msg)
 
 
 class RuleAlreadyRegisteredError(Exception):
@@ -19,9 +28,36 @@ class RuleKeyDoesNotExistError(Exception):
         super().__init__(f"Key '{key}' does not exist in the object of rule '{rule_name}'")
 
 
-class CompilationError(Exception):
-    """Exception raised when a rule compilation fails."""
+class ArgumentError(TypeError):
+    pass
 
 
-class ProcessArgumentError(Exception):
+class MissingArgumentError(ArgumentError):
+    """Exception raised when a required argument is missing."""
+
+
+def is_missing_argument_exception(e: TypeError) -> bool:
+    return (
+        re.search(r"missing \d+ required positional argument", e.args[0]) is not None
+        or re.search(r" missing \d+ required keyword-only argument", e.args[0]) is not None
+    )
+
+
+class UnexpectedKeywordArgumentError(ArgumentError):
+    """Exception raised when an unexpected keyword argument is passed."""
+
+
+def is_unexpected_keyword_argument_exception(e: TypeError) -> bool:
+    return re.search(r"got an unexpected keyword argument", e.args[0]) is not None
+
+
+class TooManyArgumentsError(ArgumentError):
+    """Exception raised when too many arguments are passed."""
+
+
+def is_too_many_arguments_exception(e: TypeError) -> bool:
+    return re.search(r"takes \d+ positional arguments? but \d+ were given", e.args[0]) is not None
+
+
+class ProcessArgumentError(ArgumentError):
     """Exception raised when a process argument fails in registry class."""
